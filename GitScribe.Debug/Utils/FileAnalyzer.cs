e6bd0f1c18226cf29f74349ef1ceab355385fb99
Gustav Hultskär <gustav.hultskar@icore.se>
2024-10-16 17:44:40 +0200

    Add IFileAnalyzer interface in Utils
    Introduce IFileAnalyzer to handle patch retrieval and concatenation in GitScribe.Debug project. This interface provides methods for analyzing file changes and generating patches for specific files in a Git repository.

---
+++ GitScribe.Debug/Utils/FileAnalyzer.cs
@@ -0,0 +1,24 @@
+﻿using LibGit2Sharp;
+using System.Text;
+
+namespace GitScribe.Debug.Utils;
+
+public class FileAnalyzer : IFileAnalyzer
+{
+   public IEnumerable<Patch> GetPatches(string repositoryPath, StatusEntry entry)
+   {
+      string filePath = Path.Combine(repositoryPath, entry.FilePath);
+
+      using var repo = new Repository(repositoryPath);
+      yield return repo.Diff.Compare<Patch>(repo.Head.Tip.Tree, repo.Index.WriteToTree(), [filePath]);
+   }
+
+   public string GetPatchContent(IEnumerable<Patch> patches)
+   {
+      StringBuilder patchContent = new();
+      foreach (var patch in patches)
+         patchContent.AppendLine(patch.Content);
+
+      return patchContent.ToString();
+   }
+}
