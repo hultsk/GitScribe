e123025aa395835faa572980e23584fb854ea84e
Gustav Hultskär <gustav.hultskar@icore.se>
2024-10-16 17:48:04 +0200

    Add IRepositoryManager interface in Utils
    Introduce a new interface, IRepositoryManager, to manage and interact with Git repositories. This includes a method to retrieve the status of the files in a given repository.

---
+++ GitScribe.Debug/Utils/IRepositoryManager.cs
@@ -0,0 +1,18 @@
+﻿using LibGit2Sharp;
+
+namespace GitScribe.Debug.Utils;
+
+/// <summary>
+/// Defines methods for managing and interacting with Git repositories.
+/// </summary>
+public interface IRepositoryManager
+{
+   /// <summary>
+   /// Retrieves the status of the files in the specified Git repository.
+   /// </summary>
+   /// <param name="repositoryPath">The file system path to the Git repository.</param>
+   /// <returns>
+   /// An enumerable collection of <see cref="StatusEntry"/> objects representing the status of files in the repository.
+   /// </returns>
+   IEnumerable<StatusEntry> RetrieveStatus(string repositoryPath);
+}
---
+++ GitScribe.Debug/Utils/RepositoryManager.cs
@@ -0,0 +1,14 @@
+﻿using LibGit2Sharp;
+
+namespace GitScribe.Debug.Utils;
+
+public class RepositoryManager : IRepositoryManager
+{
+   public IEnumerable<StatusEntry> RetrieveStatus(string repositoryPath)
+   {
+      using (var repo = new Repository(repositoryPath))
+      {
+         return repo.RetrieveStatus();
+      }
+   }
+}
