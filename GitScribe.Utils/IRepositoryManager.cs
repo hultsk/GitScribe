a721b763cfbd63ca84f912329406ce402d83301e
Gustav Hultskär <gustav.hultskar@icore.se>
2025-01-03 10:47:47 +0100

    Move methods from FileAnalyzer => RepositoryManager
    Implement methods in RepositoryManager to retrieve patches for a specific file and concatenate their content into a single string. Refactor using a class-level repository path instead.

--- GitScribe.Utils/IRepositoryManager.cs
+++ GitScribe.Utils/IRepositoryManager.cs
@@ -8,11 +8,28 @@ namespace GitScribe.Utils;
 public interface IRepositoryManager
 {
    /// <summary>
+   /// Retrieves the patches (differences) for a specified file in the Git repository.
+   /// </summary>
+   /// <param name="entry">The status entry representing the file whose patches are being retrieved.</param>
+   /// <returns>
+   /// An enumerable collection of <see cref="Patch"/> objects that represent the differences for the specified file.
+   /// </returns>
+   IEnumerable<Patch> GetPatches(StatusEntry entry);
+
+   /// <summary>
+   /// Concatenates the content of the given patches into a single string.
+   /// </summary>
+   /// <param name="patches">An enumerable collection of <see cref="Patch"/> objects.</param>
+   /// <returns>
+   /// A string containing the concatenated content of the provided patches.
+   /// </returns>
+   string GetPatchContent(IEnumerable<Patch> patches);
+
+   /// <summary>
    /// Retrieves the status of the files in the specified Git repository.
    /// </summary>
-   /// <param name="repositoryPath">The file system path to the Git repository.</param>
    /// <returns>
    /// An enumerable collection of <see cref="StatusEntry"/> objects representing the status of files in the repository.
    /// </returns>
-   IEnumerable<StatusEntry> RetrieveStatus(string repositoryPath);
+   IEnumerable<StatusEntry> RetrieveStatus();
 }
